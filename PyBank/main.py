168b7fa0ba5ff44df66f0882acc9284e77296214
Ash-Tao <ash.taowq@gmail.com>
2022-02-20 22:07:04 +1100

    new

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,46 +1,67 @@
 
-
+# import the os module, will allow us to create file paths across operating systems
 import os
+# Module for reading CSV files
 import csv
 
 ProfitLosses = []
 change = []
 totalline = 0
-lastPL = 0
+lastProfitLosses = 0
 
+# create file path
 csvpath = os.path.join('/Users','ash.tao','python-challenge','PyBank','Resources','budget_data.csv')
+# open the file
 with open(csvpath) as csvfile:
+    # read the csv file by delimiter and variable
     csvreader = csv.reader(csvfile, delimiter=',')
+    # stores the header row
     csv_header = next(csvreader)
+    # use loop to get the list of "Profit/Losses", "total months" and "average change"
     for row in csvreader:
+        # list of "Profit/Losses"
         ProfitLosses.append(int(row[1]))
+        # Total Months
         totalline = totalline + 1
-        change.append(int(row[1]) - lastPL)
-        if lastPL != 0:
-            if (int(row[1]) - lastPL) == max(change):
+        # Total changes
+        # The first result, need will be exclusive in the following steps, since the change only starts from the second record. (-354534(Feb-10) - 1088983(Jan-10))
+        # Average Change = total changes / total months
+        change.append(int(row[1]) - lastProfitLosses)
+        # find the month of the greatest increase in profits
+        if lastProfitLosses != 0:
+            if (int(row[1]) - lastProfitLosses) == max(change):
                 maxmonth = row[0]
-                #print(maxmonth)
-            if (int(row[1]) - lastPL) == min(change):
+            if (int(row[1]) - lastProfitLosses) == min(change):
                 minmonth = row[0]
-                #print(minmonth)
+        # This code will be executed first and only once in the loop.
+        # Even though it is written after others. 
+        # Because the first lastProfitLosses is set =0 and will be given a new non-0 value at the end of each loop.
         else:
+            # Exclusive the first result. (1088983(Jan-10) - 0 (no data))
             change.pop(0)
-        lastPL=int(row[1])
+        # Set ProfitLosses as lastProfitLosses for  next loop calculation
+        lastProfitLosses=int(row[1])
 
-
+# make the calculation
 totalmonths = f"Total Months: {totalline}"
+print(ProfitLosses)
 totalPL = f"Total: ${sum(ProfitLosses)}"
 averagechange = f'Average Change: ${"{:.2f}".format(sum(change)/(totalline-1))}'
 greatestinPL = f"Greatest Increase in Profits: {maxmonth} (${max(change)})"
 greatestdePL = f"Greatest Decrease in Profits: {minmonth} (${min(change)})"
-
+# make a list for the output.
 outputs = (" ","Financial Analysis","-----------------------------------",totalmonths,totalPL,averagechange,greatestinPL,greatestdePL)
 
+# use loop show the output list in the Terminal
 for output in outputs:
     print(output)
 
+
+# create file and its path
 output_file = os.path.join('/Users','ash.tao','python-challenge','PyBank','analysis','output.txt')
+# Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_file, "w") as datafile:
+    # use loop save the output list in the .txt file
     for output in outputs:
         datafile.writelines(output)
         datafile.writelines("\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,37 +1,59 @@
+# import the os module, will allow us to create file paths across operating systems
 import os
+# Module for reading CSV files
 import csv
-from traceback import print_tb
 
 votesdetails = []
 candidate = []
 totalvotes = 0
-csvpath = os.path.join('/Users','ash.tao','Desktop','PyPoll','Resources','election_data.csv')
+
+# create file path
+csvpath = os.path.join('/Users','ash.tao','python-challenge','PyPoll','Resources','election_data.csv')
+# open the file
 with open(csvpath) as csvfile:
+    # read the csv file by delimiter and variable
     csvreader = csv.reader(csvfile, delimiter=',')
+    # stores the header row
     csv_header = next(csvreader)
     for row in csvreader:
+        # have the list of all the votes
         votesdetails.append(row[2])
+        # have the total amount of votes
         totalvotes = totalvotes + 1
-
-
+# have the list of 3 candidates by removing the duplicated name from the he list of all the votes
 candidate = list(dict.fromkeys(votesdetails))
+print(candidate)
 voteresult = []
 winner = []
+# create the top part of the output list 
 toplist = [' ','Election Results','-------------------------',f'Total Votes: {totalvotes}','-------------------------']
 completelist = []
+# create the second part of the output list 
 for x in candidate:
+    # have the list of total amount of each candidate
     voteresult.append(votesdetails.count(x))
+    # total votes amount of each candidate
     eachcandidate = voteresult[candidate.index(x)]
+    # percentage of votes amount of each candidate
     eachcandidatepercent = "{:.3%}".format(eachcandidate/totalvotes)
+    # combine top part and the second part of the output list
     toplist.append(f'{x}: {eachcandidatepercent} ({eachcandidate})')
+# create the last part of the output list 
+# find the max votes amount in the list - voteresult.
+# use index find the name in list - candidate, which is related to the above maximum value 
 winner = ['-------------------------',f'Winner: {candidate[voteresult.index(max(voteresult))]}','-------------------------']
+# combine the last part of the output list
 completelist = toplist + winner
 
+# use loop show the output list in the Terminal
 for output in completelist:
     print(output)
 
-output_file = os.path.join('/Users','ash.tao','Desktop','PyPoll','analysis','output.txt')
+# create file and its path
+output_file = os.path.join('/Users','ash.tao','python-challenge','PyPoll','analysis','output.txt')
+# Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_file, "w") as datafile:
+    # use loop save the output list in the .txt file
     for output in completelist:
         datafile.writelines(output)
         datafile.writelines("\n")
